afd72b08df20a9e316abf5106143e3954695a930
abobalypse <abobalyptica99@gmail.com>
2024-02-18 11:52:09 -0500

    Slots

--- resources/script.js
+++ resources/script.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+const SYMBOL_VALUES = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: ")
@@ -43,6 +60,23 @@ const getBet = (balance, lines) => {
       console.log("Invalid bet, try again.")
     } else {
       return numberBet;
+    }
+  }
+};
+
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [[]. [], []];
+  for (let i = 0; i < COLS; i++) {
+      const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+
     }
   }
 };

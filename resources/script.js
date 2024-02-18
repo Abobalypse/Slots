4c55538af054753d873918aee7a9abdaff471071
abobalypse <abobalyptica99@gmail.com>
2024-02-18 16:35:26 -0500

    Slots

--- resources/script.js
+++ resources/script.js
@@ -74,13 +74,13 @@ const spin = () => {
 
   const reels = [];
   for (let i = 0; i < COLS; i++) {
-      reels.push([]);
-      const reelSymbols = [...symbols];
+    reels.push([]);
+    const reelSymbols = [...symbols];
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-        const selectedSymbol = reelSymbols[randomIndex];
-        reels[i].push(selectedSymbol);
-        reelSymbols.splice(randomIndex, 1);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
     }
   }
 
@@ -112,12 +112,54 @@ const printRows = (rows) => {
     }
     console.log(rowString);
   }
+};
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]]
+    }
+  }
+
+  return winnings;
+};
+
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+        console.log("You have a balance of $" + balance)
+      const numberOfLines = getNumberOfLines();
+      const bet = getBet(balance, numberOfLines);
+        balance -= bet * numberOfLines;
+      const reels = spin();
+      const rows = transpose(reels);
+      printRows(rows);
+      const winnings = getWinnings(rows, bet, numberOfLines);
+        balance += winnings;
+          console.log("You won, $" + winnings.toString());
+
+      if (balance <= 0) { 
+        console.log("You have no more money!");
+        break;
+      }
+
+      const playAgain = prompt("Would you like to play again? (y/n)? ")
+
+      if (playAgain != "y") break;
+  }  
 }
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
+game();
